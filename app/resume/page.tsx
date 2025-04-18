97b7731736e28a9b5e22ee89a5855ee6c1db0f50
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-04-18 10:10:38 +0530

    resume skeleton laoder

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,18 +1,28 @@
 "use client"
+import { useState ,useEffect} from "react";
 import Header from "../components/header";
 
 export default function Resume(){
+    const [loading, setLoading] = useState(true); 
+
+    useEffect(() => {
+      const timer = setTimeout(() => setLoading(false),1500)
+      return () => clearTimeout(timer)
+    },[])
+
     return (
       <div>
         <Header />
-        <div 
-          className="flex justify-center items-center  p-4 pt-25">
+        <div className="flex justify-center items-center  p-4 pt-25">
+          {loading && (
+            <div className="w-[800px] h-[1100px] animate-pulse bg-gray-500 rounded-xl"></div>
+          )}
           <embed
             src="gourav_resume_v1.pdf"
             width="800px"
             height="1100px"
             type="application/pdf"
-            className="rounded-xl"
+            className={`rounded-xl ${loading ? "hidden" : "block"}`}
           />
         </div>
       </div>
