9ff9f42cc5263cade89a74b4a0c6efc3f137a5df
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-05-10 00:42:58 +0530

    update project

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,4 +1,6 @@
 import Header from "../components/header";
+import HueIcon from "./project_icon/hueicon";
+import NeuronIcon from "./project_icon/neuronicon";
 
 export default function Project() {
   return (
@@ -13,9 +15,52 @@ export default function Project() {
           learning, exploration, and the joy of bringing ideas to life.
         </p>
       </div>
-      <h1 className="text-8xl font-bold tracking-tight py-10 px-10 opacity-20 text-outline">
+      <hr className="w-24 border-t-2 border-white mx-auto" />
+      <h1 className="text-8xl font-bold tracking-tight py-4 px-10 opacity-20 text-outline">
         Work in progress
       </h1>
+      <div className="flex flex-wrap justify-center gap-2 px-12 py-6">
+        <div className="max-w-sm p-6 m-2 px-10 rounded-2xl flex items-center gap-2 text-gray-500 hover:cursor-pointer hover:opacity-50 hover:bg-gray-600 hover:text-white transition duration-200">
+          <div className="text-3xl m-2">
+            <NeuronIcon />
+          </div>
+          <div>
+            <h2 className="text-xl font-semibold text-white">FINE</h2>
+            <p className="text-sm text-white">
+              One-liner description goes here.
+            </p>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-8xl font-bold tracking-tight py-4 px-10 opacity-20 text-outline">
+        Websites
+      </h1>
+      <div className="flex flex-wrap justify-center gap-2 px-12 py-6">
+        {/* card 1*/}
+        <div className="max-w-sm p-6 m-2 px-10 rounded-2xl flex items-center gap-2 text-gray-500 hover:cursor-pointer hover:opacity-50 hover:bg-gray-600 hover:text-white transition duration-200">
+          <div className="text-3xl m-2">
+            <NeuronIcon />
+          </div>
+          <div>
+            <h2 className="text-xl font-semibold text-white">Neuron</h2>
+            <p className="text-sm text-white">
+              One-liner description goes here.
+            </p>
+          </div>
+        </div>
+        {/* card 2 */}
+        <div className="max-w-sm p-6 m-2 px-10 rounded-2xl flex items-center gap-2 text-gray-500 hover:cursor-pointer hover:opacity-50 hover:bg-gray-600 hover:text-white transition duration-200">
+          <div className="text-3xl m-2">
+            <HueIcon />
+          </div>
+          <div>
+            <h2 className="text-xl font-semibold text-white">Hue</h2>
+            <p className="text-sm text-white">
+              One-liner description goes here.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
