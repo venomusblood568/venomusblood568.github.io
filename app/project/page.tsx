86444ec09ed73b7001a80763543c535a6b9f62e1
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-05-09 23:53:18 +0530

    project update

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -4,14 +4,18 @@ export default function Project() {
   return (
     <div>
       <Header />
-      <div className="flex items-center justify-center w-full h-screen">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold">Project PAGE</h1>
-          <h5 className="text-lg mt-2">
-            Still cookin&apos;... ladies and gentlemen 🍳🔥
-          </h5>
-        </div>
+      <div className="flex flex-col items-center text-center max-w-2xl mx-auto px-4 pt-20 pb-10 gap-4">
+        <h1 className="text-6xl font-bold tracking-tight py-10 px-10 opacity-40">
+          Project Showcase
+        </h1>
+        <p className="text-xl text-gray-300">
+          A collection of my favorite projects — Each build reflects a moment of
+          learning, exploration, and the joy of bringing ideas to life.
+        </p>
       </div>
+      <h1 className="text-8xl font-bold tracking-tight py-10 px-10 opacity-20 text-outline">
+        Work in progress
+      </h1>
     </div>
   );
 }
