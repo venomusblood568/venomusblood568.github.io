ef9b18a8edca461164f3f3f01fa5074edfd7af72
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-05-12 00:35:17 +0530

    project page update

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,35 +1,28 @@
 import Header from "../components/header";
-import HueIcon from "./project_icon/hueicon";
-import NeuronIcon from "./project_icon/neuronicon";
 
 export default function Project() {
   return (
     <div>
       <Header />
-      <div className="flex flex-col items-center text-center max-w-2xl mx-auto px-4 pt-20 pb-10 gap-4">
-        <h1 className="text-6xl font-bold tracking-tight py-10 px-10 opacity-40">
-          Project Showcase
+      <div className="flex flex-col items-center text-center max-w-2xl mx-auto px-4 pt-30 pb-10 gap-4">
+        <h1 className="text-6xl font-bold tracking-tight opacity-80">
+          Projects
         </h1>
-        <p className="text-xl text-gray-300">
-          A collection of my favorite projects — Each build reflects a moment of
-          learning, exploration, and the joy of bringing ideas to life.
+        <p className="text-xl text-gray-500">
+          A collection of projects I've created or currently maintain.
         </p>
+        <hr className="w-24 border-t-2 border-gray-600" />
       </div>
-      <hr className="w-24 border-t-2 border-white mx-auto" />
+
       <h1 className="text-8xl font-bold tracking-tight py-4 px-10 opacity-20 text-outline">
         Work in progress
       </h1>
       <div className="flex flex-wrap justify-center gap-2 px-12 py-6">
         {/* 1st card */}
         <div className="max-w-sm p-6 m-2 px-10 rounded-2xl flex items-center gap-2 text-gray-500 hover:cursor-pointer hover:opacity-50 hover:bg-gray-600 hover:text-white transition duration-200">
-          <div className="text-3xl m-2">
-            <HueIcon />
-          </div>
           <div>
             <h2 className="text-xl font-semibold">Fine</h2>
-            <p className="text-sm">
-              Cooking something cool.....
-            </p>
+            <p className="text-sm">Cooking something cool.....</p>
             <div className="mt-2 flex gap-4 text-sm">
               <a
                 href="sss"
@@ -58,9 +51,6 @@ export default function Project() {
       <div className="flex flex-wrap justify-center gap-2 px-12 py-6">
         {/* card 1*/}
         <div className="max-w-sm p-6 m-2 px-10 rounded-2xl flex items-center gap-2 text-gray-500 hover:cursor-pointer hover:opacity-50 hover:bg-gray-600 hover:text-white transition duration-200">
-          <div className="text-3xl m-2">
-            <NeuronIcon />
-          </div>
           <div>
             <h2 className="text-xl font-semibold">Neuron</h2>
             <p className="text-sm">
@@ -88,9 +78,6 @@ export default function Project() {
         </div>
         {/* card 2 */}
         <div className="max-w-sm p-6 m-2 px-10 rounded-2xl flex items-center gap-2 text-gray-500 hover:cursor-pointer hover:opacity-50 hover:bg-gray-600 hover:text-white transition duration-200">
-          <div className="text-3xl m-2">
-            <HueIcon />
-          </div>
           <div>
             <h2 className="text-xl font-semibold">Hue</h2>
             <p className="text-sm">
@@ -112,6 +99,71 @@ export default function Project() {
                 rel="noopener noreferrer"
               >
                 &gt; Live Demo
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-8xl font-bold tracking-tight py-4 px-10 opacity-20 text-outline">
+        Chrome Extension
+      </h1>
+      <div className="flex flex-wrap justify-center gap-2 px-12 py-6">
+        {/* card 1*/}
+        <div className="max-w-sm p-6 m-2 px-10 rounded-2xl flex items-center gap-2 text-gray-500 hover:cursor-pointer hover:opacity-50 hover:bg-gray-600 hover:text-white transition duration-200">
+          <div>
+            <h2 className="text-xl font-semibold">Link Vault</h2>
+            <p className="text-sm">
+              A handy extension to save and organize all your links for later
+              visits.
+            </p>
+            <div className="mt-2 flex gap-4 text-sm">
+              <a
+                href="https://github.com/venomusblood568/linkvault"
+                className="hover:underline hover:scale-110"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                &gt; GitHub
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* card 2 */}
+        <div className="max-w-sm p-6 m-2 px-10 rounded-2xl flex items-center gap-2 text-gray-500 hover:cursor-pointer hover:opacity-50 hover:bg-gray-600 hover:text-white transition duration-200">
+          <div>
+            <h2 className="text-xl font-semibold">Hyper Flow</h2>
+            <p className="text-sm">
+              Easily adjust YouTube playback speed from 0.5x to 4x with a
+              simple, user-friendly interface.
+            </p>
+            <div className="mt-2 flex gap-4 text-sm">
+              <a
+                href="https://github.com/venomusblood568/HyperFlow_playcontrol"
+                className="hover:underline hover:scale-105"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                &gt; GitHub
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* card 3 */}
+        <div className="max-w-sm p-6 m-2 px-10 rounded-2xl flex items-center gap-2 text-gray-500 hover:cursor-pointer hover:opacity-50 hover:bg-gray-600 hover:text-white transition duration-200">
+          <div>
+            <h2 className="text-xl font-semibold">GitaVerses</h2>
+            <p className="text-sm">
+              GitaVerse Explorer shows you a random Bhagavad Gita verse with one
+              click, bringing daily spiritual insight to your browser.
+            </p>
+            <div className="mt-2 flex gap-4 text-sm">
+              <a
+                href="https://github.com/venomusblood568/Project5/tree/main/GitaVerses"
+                className="hover:underline hover:scale-105"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                &gt; GitHub
               </a>
             </div>
           </div>
